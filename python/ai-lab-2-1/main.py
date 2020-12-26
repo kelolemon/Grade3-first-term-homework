c72fd228caf7195179dced9a9c5aee1d7e2af3ee
kelolemon <cutesinapis@gmail.com>
2020-12-26 23:33:15 +0800

    usual update

--- python/ai-lab-2-1/main.py
+++ python/ai-lab-2-1/main.py
@@ -4,24 +4,21 @@ from sklearn.svm import LinearSVC
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
 
 
 # 3. build svm.LinearSVC
 # draw the decision boundary
-def plot_decision_boundary(model, axis):
-    x0, x1 = np.meshgrid(
-        np.linspace(axis[0], axis[1], int((axis[1] - axis[0]) * 100)).reshape(-1, 1),
-        np.linspace(axis[2], axis[3], int((axis[3] - axis[2]) * 100)).reshape(-1, 1)
-    )
-    X_new = np.c_[x0.ravel(), x1.ravel()]
-
-    y_predict = model.predict(X_new)
-    zz = y_predict.reshape(x0.shape)
-
-    from matplotlib.colors import ListedColormap
+def plot_decision_boundary(model, x_train):
+    h = .02
+    x_min, x_max = x_train[:, 0].min() - .5, x_train[:, 0].max() + .5
+    y_min, y_max = x_train[:, 1].min() - .5, x_train[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    zz = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    zz = zz.reshape(xx.shape)
     custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
-
-    plt.contourf(x0, x1, zz, cmap=custom_cmap)
+    plt.pcolormesh(xx, yy, zz, cmap=custom_cmap, shading='auto')
 
 
 def main():
@@ -30,31 +27,21 @@ def main():
     data, label = make_blobs(n_samples=1000, n_features=2, centers=2, cluster_std=[2.0, 2.0])
 
     # 2. draw
-
+    plt.subplot(211)
     plt.scatter(data[:, 0], data[:, 1], c=label)
-    plt.show()
+    plt.subplot(212)
 
     # set the test and train data and stand the data
-    rate_test_of_all = 0.2
-    test_sum = int(rate_test_of_all * len(data))
-    data_test = data[-test_sum:]
-    data_train = data[:-test_sum]
-    label_test = label[-test_sum:]
-    label_train = label[:-test_sum]
-    standardScaler = StandardScaler()
-    len_test = int(len(data) * 0.2)
-    standardScaler.fit(data_train)
-    data_standard = standardScaler.transform(data_train)
-    standardScaler.fit(data_test)
-    data_test = standardScaler.transform(data_test)
+    data_train, data_test, label_train, label_test = train_test_split(data, label, test_size=1 / 12, random_state=0)
+
     # Hard Margin SVM : C = 10**9  Soft Margin SVM: C = 0.01
-    svc = LinearSVC(C=10 ** 9)
-    svc.fit(data_standard, label_train)
+    svc = LinearSVC(C=0.01)
+    svc.fit(data_train, label_train)
     print("The accuracy is:")
     print(accuracy_score(label_test, svc.predict(data_test)))
-    plot_decision_boundary(svc, axis=[-3, 3, -3, 3])
-    plt.scatter(data_standard[label_train == 0, 0], data_standard[label_train == 0, 1], color='red')
-    plt.scatter(data_standard[label_train == 1, 0], data_standard[label_train == 1, 1], color='blue')
+    plot_decision_boundary(svc, data_train)
+    plt.scatter(data_train[label_train == 0, 0], data_train[label_train == 0, 1], color='red')
+    plt.scatter(data_train[label_train == 1, 0], data_train[label_train == 1, 1], color='blue')
     plt.show()
 
 
