63b184e6307e8e4e71d76a752530b0626f320f06
kelolemon <cutesinapis@gmail.com>
2020-12-28 23:03:57 +0800

    usual update

--- python/ai-lab-3/main.py
+++ python/ai-lab-3/main.py
@@ -19,13 +19,14 @@ def dbscan(x_set):
     plt.scatter(x_set[:, 0], x_set[:, 1], c=clf.labels_)
     plt.show()
     print('dbscan silhouette score:{}'.format(silhouette_score(x_set, y_predict)))
-w
+
 
 def main():
     data_set = np.load('Data_for_Cluster.npz')
     x_set = data_set['X']
     y_set = data_set['labels_true']
     plt.scatter(x_set[:, 0], x_set[:, 1], c=y_set)
+    plt.show()
     dbscan(x_set)
     k_means(x_set)
 
--- python/ai-lab-4/ai-lab-4.py
+++ python/ai-lab-4/ai-lab-4.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -13,18 +12,39 @@ def one_hot(labels):
 
 def cnn_model(input_shape):
     model = tf.keras.Sequential()
-    # the first
-    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=5, strides=(1, 1),
+    # the first Convolutional layer and pooling layer
+    # kernel size is defined classically 5 * 5, strides is 1 and using the relu algorithm, using 0 to pade the margin.
+    # the input shape is (image_height, image_width, color_channels), the pictures is black and white,
+    # so the color_channels is 1, the image height and width is output by former initialize we can discover that is
+    # 28 * 28, so the input shape is (28, 28, 1)
+    # filters is 32 means that there are 32 kernels and the output depths are 32.
+    # using classical 2 * 2 pool size pooling layer
+    # after these two layers, the output is (14, 14, 32)
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=5, strides=(1, 1),
                                      padding='same', activation=tf.nn.relu, input_shape=input_shape))
     model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
-    model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu))
+    # the second Convolutional layer and pooling layer
+    # kernel size is defined classically 3 * 3, strides is 1 and using the relu algorithm, using 0 to pade the margin.
+    # the input shape is 14 * 14 * 32
+    # filters is 64 means that there are 64 kernels and the output depths are 64.
+    # using classical 2 * 2 pool size pooling layer
+    # after these two layers, the output is (7， 7， 64)
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu))
     model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
-    model.add(tf.keras.layers.Dropout(0.25))
+    # using dropout layer to allow mistakes and predict over fit
+    # model.add(tf.keras.layers.Dropout(0.25))
+    # using flatten layer to make multi dimensions data to one dimension data.
+    # this layer is to make preparation for the later full connecting layers
+    # after this layer the output is (7 * 7 * 64) equals (3136)
     model.add(tf.keras.layers.Flatten())
-
+    # the first full-connecting layer, input is 3136, output is 128
     model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dropout(0.5))
+    # using dropout layer to allow mistakes and predict over fit
+    # model.add(tf.keras.layers.Dropout(0.5))
+    # the second full-connecting layer, input is 128, output is 10
+    # the output is 10. the reason is that the answer is between 0 and 9.
     model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+    # return the model
     return model
 
 
@@ -43,6 +63,7 @@ def train_model(train_images, train_labels, test_images, test_labels):
     # create model
     # using cnn model
     model = cnn_model(input_shape=(28, 28, 1))
+    print(model.summary())
     model.compile(metrics=['accuracy'], optimizer="adam", loss="categorical_crossentropy")
     model.fit(x=train_images, y=train_labels, epochs=5)
     test_loss, test_acc = model.evaluate(x=test_images, y=test_labels)
